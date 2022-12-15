1fcf84b5fbbd36e3fc0a9a86958901e51d3d39a5
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-15 20:55:31 -0300

    populated seeds with few movies and rendered flash alerto to righ bottom section of the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+Movie.create(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9)
+Movie.create(
+  title: "The Shawshank Redemption",
+  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  rating: 8.7)
+Movie.create(
+  title: "Titanic",
+  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+  rating: 7.9)
+Movie.create(
+  title: "Ocean's Eight",
+  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+  rating: 7.0)
+Movie.create(
+  title: "Troll",
+  overview: "Deep inside the mountain of Dovre, something gigantic awakens after being trapped for a thousand years. Destroying everything in its path, the creature is fast approaching the capital of Norway. But how do you stop something you thought only existed in Norwegian folklore?",
+  poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9z4jRr43JdtU66P0iy8h18OyLql.jpg",
+  rating: 6.8)
+Movie.create(
+  title: "Avatar: The Way of Water",
+  overview: "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/94xxm5701CzOdJdUEdIuwqZaowx.jpg",
+  rating: 8.2)
+Movie.create(
+  title: "The Woman King",
+  overview: "The story of the Agojie, the all-female unit of warriors who protected the African Kingdom of Dahomey in the 1800s with skills and a fierceness unlike anything the world has ever seen, and General Nanisca as she trains the next generation of recruits and readies them for battle against an enemy determined to destroy their way of life.",
+  poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lQMJHnHxUyj8kJlC2YOKNuzuwMP.jpg",
+  rating: 7.9)
+
+puts "Mission accomplished!"
