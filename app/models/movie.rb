73a0476cdeaf2128e606c119035e24ae278e0f7b
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-15 12:11:01 -0300

    added validations to movie, list and bookmark models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, length: { in: 25..150 }
+  validates :poster_url, presence: true, uniqueness: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
