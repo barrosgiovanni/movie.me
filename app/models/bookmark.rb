cddcc2f40fa35e3d71b08ff32852c0f1c1bc0a88
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-16 18:57:17 -0300

    added template to lists#show

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, presence: true, length: { minimum: 10 }
+  validates :comment, presence: true, length: { minimum: 20 }
 end
