a99d3a3dfc2edda4467e1e1808ca79de7538e985
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-20 23:14:30 -0300

    added movies#index page and gave it a bit of styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # root defined
   root to: "pages#home"
 
-  get 'lists',           to: 'lists#index'
-  get 'lists/new',       to: 'lists#new'
-  post 'lists',          to: 'lists#create'
-  get 'lists/:id',       to: 'lists#show',        as: :list
-  delete 'lists/:id',    to: 'lists#destroy'
+  # get 'lists',           to: 'lists#index'
+  # get 'lists/new',       to: 'lists#new'
+  # post 'lists',          to: 'lists#create'
+  # get 'lists/:id',       to: 'lists#show',        as: :list
+  # delete 'lists/:id',    to: 'lists#destroy'
+
+  resources :lists, except: [:edit, :update]
+
+  get 'movies', to: 'movies#index'
 end
