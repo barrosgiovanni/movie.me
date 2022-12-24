bc90156c28de48b6b19eea9bbf82af11b209f832
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-24 16:02:27 -0300

    added show more & show less button on movie card description

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,8 @@ def movies_dataset
   # assigning page equals to 1, this value will be changed as we iterate to create the movie's instances ...
   page = 1
 
-  # iterating to create movies and adding +1 to pages so we can create movies in the first 50 pages ...
-  while page <= 50
+  # iterating to create movies and adding +1 to pages so we can create movies in the first 300 pages ...
+  while page <= 500
     top_movies = RestClient.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_data[:key]}&language=en-US&page=#{page}")
     top_movies_array = JSON.parse(top_movies)["results"]
     top_movies_array.each do |movie|
@@ -28,7 +28,8 @@ def movies_dataset
         title: movie["title"],
         overview: movie["overview"],
         poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/#{movie["poster_path"]}",
-        rating: movie["vote_average"])
+        rating: movie["vote_average"]
+      )
     end
     page += 1
   end
