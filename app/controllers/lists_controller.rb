a9c6aaa686b4cdf998769e8d315e433f3e761d54
Giovanni Barros <barrosgiovanni@icloud.com>
2023-01-11 20:53:47 -0300

    added pundit policy to movies and lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,20 +2,23 @@ class ListsController < ApplicationController
   before_action :set_list, only: [:show, :destroy]
 
   def index
-    @lists = current_user.lists
+    @lists = policy_scope(List)
   end
 
   def show
+    authorize(@list)
     @bookmark = Bookmark.new
   end
 
   def new
     @list = List.new
+    authorize(@list)
   end
 
   def create
     @list = List.new(list_params)
     @list.user = current_user
+    authorize(@list)
     if @list.save
       redirect_to list_path(@list)
     else
@@ -24,6 +27,7 @@ class ListsController < ApplicationController
   end
 
   def destroy
+    authorize(@list)
     @list.destroy
     redirect_to lists_path, status: :see_other
   end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,10 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @pagy, @movies = pagy(Movie.search_by_title_and_overview(params[:query]))
+      @pagy, @movies = pagy(policy_scope(Movie).search_by_title_and_overview(params[:query]))
     else
-      @pagy, @movies = pagy(Movie.all)
+      @pagy, @movies = pagy(policy_scope(Movie).all)
     end
-
-    authorize
   end
 
   def show
